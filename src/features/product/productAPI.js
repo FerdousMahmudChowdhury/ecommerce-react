efae31b3d561120a1196c79701d908a5d1a61f69
FerdousMahmudChowdhury <ferdous.m.chowdhury@gmail.com>
2023-12-07 20:32:45 +0600

    Part 5 : Setup Redux of Products/ JSON-server/ Products Filters

---
+++ src/features/product/productAPI.js
@@ -0,0 +1,26 @@
+// A mock function to mimic making an async request for data
+export function fetchAllProducts() {
+  return new Promise(async (resolve) => {
+    //TODO: we will not hardcode server url here
+    const response = await fetch('http://localhost:8080/products')
+    const data = await response.json()
+    resolve({ data })
+  }
+  );
+}
+
+export function fetchProductsByFilters(filter) {
+  //filter = "category":"smartphones"
+  let queryString = '';
+  for(let key in filter){
+    queryString += `${key}=${filter[key]}&`
+  }
+
+  return new Promise(async (resolve) => {
+    //TODO: we will not hardcode server url here
+    const response = await fetch('http://localhost:8080/products?'+queryString)
+    const data = await response.json()
+    resolve({ data })
+  }
+  );
+}
--- steps.js
+++ steps.js
@@ -1,5 +1,9 @@
 /*****
  * npx create-react-app e-commerce --template redux
  * npm install @headlessui/react @heroicons/react
+ * npm i -g json-server
+ * json-server --watch data.json --port 8080
+ * Quokka.js
+ * >quokka 
  * 
  * /
