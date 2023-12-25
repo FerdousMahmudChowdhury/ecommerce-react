b0755a348ab66d16697283dbc6556ea38bd28625
FerdousMahmudChowdhury <ferdous.m.chowdhury@gmail.com>
2023-12-25 17:41:59 +0600

    Part 26 : email and reset password flow ( backend part 10)

--- src/features/auth/component/ForgotPassword.js
+++ src/features/auth/component/ForgotPassword.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { resetPasswordRequestAsync, selectMailSent } from '../authSlice';
 
 export default function ForgotPassword() {
+
+    const mailSent = useSelector(selectMailSent);
+    const dispatch = useDispatch()
     const { register, handleSubmit, formState: { errors }, } = useForm()
 
     console.log(errors)
@@ -13,7 +18,7 @@ export default function ForgotPassword() {
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img
                         className="mx-auto h-10 w-auto"
-                                                // TODO : logo
+                        // TODO : logo
 
                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                         alt="Your Company"
@@ -28,7 +33,7 @@ export default function ForgotPassword() {
                         noValidate
                         onSubmit={handleSubmit((data) => {
                             console.log(data)
-                            // TODO : implementations on bancked with email
+                            dispatch(resetPasswordRequestAsync(data.email))
                         })}
                         className="space-y-6" action="#" method="POST">
                         <div>
@@ -48,7 +53,12 @@ export default function ForgotPassword() {
 
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
-                                <p className='text-red-500'>{errors?.email?.message}</p>
+                                {errors.email && (
+                                    <p className="text-red-500">{errors.email.message}</p>
+                                )}
+                                {mailSent && (
+                                    <p className="text-green-500">Mail Sent</p>
+                                )}
                             </div>
                         </div>
 
